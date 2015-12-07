219752a2ff0db169c2f2f960b006685428adf21e
Martijn de Vos <martijn@code-up.nl>
2015-12-07 12:01:20 +0100

    WX3 changes
    Fixed popup menus in panels on OS X
    
    Added conditional import to tribler.py
    
    WX3 changes
    
    Fixed popup menus in panels on OS X
    
    Added conditional import to tribler.py
    
    Fix dispersy
    
    Removed growable row addition
    
    Why did this get added??
    
    Fixed pointer
    
    Made the wx import conditional in the tests

--- Tribler/Main/vwxGUI/list_details.py
+++ Tribler/Main/vwxGUI/list_details.py
@@ -1481,8 +1481,6 @@ class ChannelDetails(AbstractDetails):
         self.detailsTab.SetBackgroundColour(wx.WHITE)
 
         fgSizer = wx.FlexGridSizer(0, 2, 3, 10)
-        fgSizer.AddGrowableCol(1)
-        fgSizer.AddGrowableRow(6)
 
         titles = ['Name', 'Description', 'Torrents', 'Latest update', 'Favorite votes']
         for title in titles:
@@ -1491,6 +1489,8 @@ class ChannelDetails(AbstractDetails):
             control2_name = title.lower().replace(' ', '')
             setattr(self, control1_name, control1)
             setattr(self, control2_name, control2)
+        
+        fgSizer.AddGrowableCol(1)
 
         self.detailsSizer.Add(fgSizer, 1, wx.EXPAND)
         self.detailsTab.Layout()
@@ -1572,8 +1572,6 @@ class PlaylistDetails(AbstractDetails):
         self.detailsTab.SetBackgroundColour(wx.WHITE)
 
         fgSizer = wx.FlexGridSizer(0, 2, 3, 10)
-        fgSizer.AddGrowableCol(1)
-        fgSizer.AddGrowableRow(6)
 
         titles = ['Name', 'Description', 'Torrents']
         for title in titles:
@@ -1602,6 +1600,9 @@ class PlaylistDetails(AbstractDetails):
         tSizer.Add(self.thumbnails, 0, wx.ALIGN_RIGHT | wx.ALIGN_TOP | wx.EXPAND)
         self.detailsSizer.Add(tSizer, 1, wx.EXPAND)
         self.thumbnails.Show(False)
+        
+        fgSizer.AddGrowableCol(1)
+
         self.detailsTab.Layout()
 
         self.Thaw()
