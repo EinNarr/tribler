fc3389673a6b5a8f64ae256cab81f0a0125ef165
Martijn de Vos <martijn@code-up.nl>
2015-12-10 16:55:28 +0100

    Task icon should now be updated with download/upload speeds
    Minor change
    
    Added line
    
    Added conditional statement
    
    Fixed icon parameter in ABCTaskBarIcon
    
    Fixed call
    
    Fixed capital letter in GetIcon method call
    
    Only update the tooltip on Windows
    
    Added check to see if the frame still exists

--- Tribler/Main/tribler_main.py
+++ Tribler/Main/tribler_main.py
@@ -54,7 +54,7 @@ from Tribler.Main.Utility.GuiDBHandler import GUIDBProducer, startWorker
 from Tribler.Main.Utility.compat import (convertDefaultDownloadConfig, convertDownloadCheckpoints, convertMainConfig,
                                          convertSessionConfig)
 from Tribler.Core.Utilities.install_dir import determine_install_dir
-from Tribler.Main.Utility.utility import Utility
+from Tribler.Main.Utility.utility import Utility, get_download_upload_speed
 from Tribler.Main.globals import DefaultDownloadStartupConfig
 from Tribler.Main.vwxGUI.GuiImageManager import GuiImageManager
 from Tribler.Main.vwxGUI.GuiUtility import GUIUtility, forceWxThread
@@ -532,6 +532,11 @@ class ABCApp(object):
     def sesscb_states_callback(self, dslist):
         if not self.ready:
             return 5.0, []
+
+        # update tray icon
+        total_download, total_upload = get_download_upload_speed(dslist)
+        if self.frame and self.frame.tbicon:
+            self.frame.tbicon.updateTooltip(total_download, total_upload)
 
         wantpeers = []
         self.ratestatecallbackcount += 1
