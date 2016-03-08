e209a66ac125355f48eee48e32a9d6f74545faa6
Martijn de Vos <martijn@code-up.nl>
2016-03-08 15:44:44 +0100

    Totally refactored I2I tests using deferred objects
    Forgot the 1st argument to i2i_server and the stop method invocation

--- Tribler/Main/tribler_main.py
+++ Tribler/Main/tribler_main.py
@@ -109,6 +109,7 @@ class ABCApp(object):
         self.done = False
         self.frame = None
         self.upgrader = None
+        self.i2i_server = None
 
         # DISPERSY will be set when available
         self.dispersy = None
@@ -200,7 +201,8 @@ class ABCApp(object):
             if not ALLOW_MULTIPLE:
                 # Put it here so an error is shown in the startup-error popup
                 # Start server for instance2instance communication
-                Instance2InstanceServer(self.utility.read_config('i2ilistenport'), self.i2ithread_readlinecallback)
+                self.i2i_server = Instance2InstanceServer(self.utility.read_config('i2ilistenport'))
+                self.i2i_server.start(self.i2ithread_readlinecallback)
 
             self.splash.tick('GUIUtility register')
             self.guiUtility.register()
@@ -865,6 +867,9 @@ class ABCApp(object):
         bm = self.gui_image_manager.getImage(u'closescreen.png')
         self.closewindow = GaugeSplash(bm, "Closing...", 6)
         self.closewindow.Show()
+
+        if self.i2i_server:
+            self.i2i_server.stop()
 
         self._logger.info("main: ONEXIT")
         self.ready = False
